940f228328b6f83e6c04178c34aa60e836901b0e
EMarshall <emilyjmarshall121@gmail.com>
2021-04-16 07:06:58 -0700

    Added user routes and user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,56 @@
+const {User} = require('../models');
+
+const userController = {
+  // Get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: 'reactions',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({_id: -1})
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // Get one user by their ID
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+    .populate({
+      path: 'reactions',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+  },
+
+  // Create a new user
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch (err => res.json(err));
+  },
+
+  // Update user by id
+  updateUser ({ params, body}, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {new: true, runValidators: true})
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({message: 'No user was found with that id. Please try again.'});
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+  }
+};
+
+module.exports = userController;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser
+} = require('../../controllers/user-controller');
+
+// Routes for /api/users
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createUser)
+
+  // Routes for /api/users/:id
+  router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+    module.exports = router;
