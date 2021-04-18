4d97a5021f448aefe612611284384b7bd8c61f28
EMarshall <emilyjmarshall121@gmail.com>
2021-04-18 13:04:52 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# social-network
+# NoSQL Challenge: Social Network
+
+## Overview
+This challenge used MongoDB, Mongoose, and Express.JS to build a social network API which would allow users to make friend lists, post thoughts, and react to their friends thoughts. 
+
+## Technology Used
+- Express.JS
+- MongoDB
+- Mongoose
+
+## Installation
+1. Clone Repository
+2. NPM I in the command line
+3. npm start
+
+## Walkthrough Videos
+[User Routes](https://screencast-o-matic.com/watch/crfFDNVnsE2)
+[Thought Routes](https://screencast-o-matic.com/watch/crfFDKVns5v)
+[Friend and Reaction Routes](https://screencast-o-matic.com/watch/crfFDJVns5Q)
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -19,7 +19,7 @@ const userController = {
 
   // Get one user by their ID
   getUserById({ params }, res) {
-    User.findOne({ _id: params.id })
+    User.findOne({ _id: params.userId })
     .populate({
       path: 'reactions',
       select: '-__v'
@@ -41,7 +41,7 @@ const userController = {
 
   // Update user by id
   updateUser ({ params, body}, res) {
-    User.findOneAndUpdate({ _id: params.id }, body, {new: true, runValidators: true})
+    User.findOneAndUpdate({ _id: params.userId }, body, {new: true, runValidators: true})
     .then(dbUserData => {
       if (!dbUserData) {
         res.status(404).json({message: 'No user was found with that id. Please try again.'});
@@ -54,7 +54,7 @@ const userController = {
 
   // Delete a user
   deleteUser({ params }, res) {
-    User.findOneAndDelete({ _id: params.id })
+    User.findOneAndDelete({ _id: params.userId })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => res.json(err));
   },
